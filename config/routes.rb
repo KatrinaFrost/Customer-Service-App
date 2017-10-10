9d7ce1f16acaffe7c303f1a205529208f812fbd0
KatrinaFrost <katrinalfrost@hotmail.com>
2017-10-10 11:35:10 +1100

    Routing and Links update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+  get '/chats' => 'chats#index'
+
+  resources :chats
+
   resources :tickets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
